9b33f244d314fe14c23a7ed6173ffd23fc26644a
Joey Fiddler <joseph.fiddler@students.fhu.edu>
2014-05-03 16:10:48 -0500

    99% done for real

--- js/script.js
+++ js/script.js
@@ -7,79 +7,38 @@ $(document).ready(function () {
 	$('.toggle-menu').jPushMenu();
 
 
-	/*$('#skillsScroll').click(function () {
+	$('#skillsScroll').click(function () {
 		$('html, body').animate({
-			scrollTop: $('#skills').offset().top }, 2000);
+			scrollTop: $('#skills').offset().top }, 1000);
 	});
 
 	$('#educationExperienceScroll').click(function () {
 		$('html, body').animate({
-			scrollTop: $('#education-experience').offset().top }, 2000);
+			scrollTop: $('#education-experience').offset().top }, 1000);
 	});
 
 	$('#worksScroll').click(function () {
 		$('html, body').animate({
-			scrollTop: $('#works').offset().top }, 2000);
+			scrollTop: $('#works').offset().top }, 1000);
 	});
 
 	$('#hobbiesScroll').click(function () {
 		$('html, body').animate({
-			scrollTop: $('#hobbies').offset().top }, 2000);
+			scrollTop: $('#hobbies').offset().top }, 1000);
 	});
 
 	$('#socialScroll').click(function () {
 		$('html, body').animate({
-			scrollTop: $('#social').offset().top }, 2000);
-	});*/
+			scrollTop: $('#social').offset().top }, 1000);
+	});
 
-	//$('#home').height(beginningWindowHeight);
 	$('#intro-row').css("margin-top", halfBeginningHeight - $('#logo-row').height());
 
-	//$('#skills').height(beginningWindowHeight);
-	//$('#skill-row').css("margin-top", halfBeginningHeight / 2.5);
-
-	//$('#education-experience').height(beginningWindowHeight);
 	$('#education-row').css("padding-top", halfBeginningHeight / 4);
 	$('#experience-row').css("margin-top", halfBeginningHeight / 2);
 
-	//$('#works').height(beginningWindowHeight);
 	$('#works h1').css("margin-top", beginningWindowHeight / 5);
-	//$('#works h1').css("margin-bottom", beginningWindowHeight / 5);
 
-	//$('#hobbies').height(beginningWindowHeight);
-
-	//$('#social').height(beginningWindowHeight);
 	$('#social h1').css("margin-top", beginningWindowHeight / 4);
 	$('#social h1').css("margin-bottom", beginningWindowHeight / 5);
-
-	/*$(window).resize(function() {
-		var resizedWindowHeight = $(window).height();
-		var halfResizedHeight = $(window).height() / 2;
-
-		$('#home').height(resizedWindowHeight);
-		$('#intro-row').css("margin-top", halfResizedHeight - $('#logo-row').height());
-
-		$('#skills').height(resizedWindowHeight);
-		//$('#skill-row').css("margin-top", halfBeginningHeight / 2.5);
-
-		$('#education-experience').height(resizedWindowHeight);
-		$('#education-row').css("padding-top", halfResizedHeight / 4);
-		$('#experience-row').css("margin-top", halfResizedHeight / 2);
-
-
-		//$('#works').height(resizedWindowHeight);
-		//$('#works h1').css("margin-top", resizedWindowHeight / 5);
-		//$('#works h1').css("margin-bottom", resizedWindowHeight / 5.5);
-
-		$('#hobbies').height(resizedWindowHeight);
-
-		$('#social').height(resizedWindowHeight);
-		$('#social h1').css("margin-top", resizedWindowHeight / 4);
-		$('#social h1').css("margin-bottom", resizedWindowHeight / 5.5);
-	});*/
-	
 });
-
-/*function setHomeHeight (windowHeight, halfHeight, element) {
-	element.height(windowHeight)
-}*/
